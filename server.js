b33af150fab3186e9acc9d1d1ef4a8aab2a43754
AnupVeerapur <anupsveerapur@gmail.com>
2020-06-06 09:34:40 +0530

    publish

---
+++ server.js
@@ -0,0 +1,20 @@
+const expconstress = require('express');
+const compression = require('compression');
+const path = require('path');
+
+const app = express();
+
+app.use(compression());
+
+app.use('/static', express.static("./build/static/"));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: './build/' });
+});
+
+const PORT = process.env.PORT || 3005;
+
+app.listen(PORT, '0.0.0.0', (err) => {
+    if (err) { console.log(err); }
+    console.info(`==> 🌎 app listening on http://localhost:${PORT}.`);
+});
